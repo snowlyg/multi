009e4b2d817392f2c6639d7c2b6a53c34aa0460e
snowlyg <569616226@qq.com>
2021-05-08 09:17:42 +0800

    修改token生成逻辑

--- verifier.go
+++ verifier.go
@@ -1,8 +1,11 @@
 package multi
 
 import (
+	"bytes"
+	"encoding/base64"
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/bwmarrin/snowflake"
 	"github.com/kataras/iris/v12/context"
@@ -105,7 +108,10 @@ func GetToken() (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("mutil: create token %w", err)
 	}
-	return node.Generate().Base64(), nil
+	now := Base64Encode([]byte(time.Now().Local().Format(time.RFC3339)))
+	nodeId := Base64Encode(node.Generate().Bytes())
+	token := joinParts(nodeId, now)
+	return string(token), nil
 }
 
 func (v *Verifier) Verify(validators ...TokenValidator) context.Handler {
@@ -122,4 +128,35 @@ func (v *Verifier) Verify(validators ...TokenValidator) context.Handler {
 		ctx.Values().Set(verifiedTokenContextKey, verifiedToken)
 		ctx.Next()
 	}
+}
+
+var (
+	sep    = []byte(".")
+	pad    = []byte("=")
+	padStr = string(pad)
+)
+
+func joinParts(parts ...[]byte) []byte {
+	return bytes.Join(parts, sep)
+}
+
+func Base64Encode(src []byte) []byte {
+	buf := make([]byte, base64.URLEncoding.EncodedLen(len(src)))
+	base64.URLEncoding.Encode(buf, src)
+
+	return bytes.TrimRight(buf, padStr) // JWT: no trailing '='.
+}
+
+// Base64Decode decodes "src" to jwt base64 url format.
+// We could use the base64.RawURLEncoding but the below is a bit faster.
+func Base64Decode(src []byte) ([]byte, error) {
+	if n := len(src) % 4; n > 0 {
+		// JWT: Because of no trailing '=' let's suffix it
+		// with the correct number of those '=' before decoding.
+		src = append(src, bytes.Repeat(pad, 4-n)...)
+	}
+
+	buf := make([]byte, base64.URLEncoding.DecodedLen(len(src)))
+	n, err := base64.URLEncoding.Decode(buf, src)
+	return buf[:n], err
 }
